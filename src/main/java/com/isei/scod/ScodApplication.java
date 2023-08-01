bd70b54d609d36390ae013603d0f0d6bf35b484d
MarcoGit93 <marco.cinelli@gmail.com>
2023-08-01 16:45:17 +0200

    Commit iniziale

---
+++ src/main/java/com/isei/scod/ScodApplication.java
@@ -0,0 +1,13 @@
+package com.isei.scod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScodApplication.class, args);
+	}
+
+}
