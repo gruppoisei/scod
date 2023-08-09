d7e276d0669cee1a409644262c64166b17937ed2
MarcoGit93 <marco.cinelli93@gmail.com>
2023-08-09 15:16:58 +0200

    Servizio refresh token

--- src/main/java/com/isei/scod/ConfigSecurity/AuthController.java
+++ src/main/java/com/isei/scod/ConfigSecurity/AuthController.java
@@ -5,10 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthController {
@@ -21,14 +18,32 @@ public class AuthController {
         this.tokenService = tokenService;
     }
 
+
+    @PostMapping("/refresh")
+    public LoginResponseDTO refreshToken( Authentication authentication,@RequestBody String refToken) {
+        LoginResponseDTO authResp = new LoginResponseDTO();
+       if(!tokenService.authToken(authentication,refToken)){
+           return null;
+       }
+        authResp = token(authentication);
+        return authResp;
+    }
+
     @PostMapping("/token")
     public LoginResponseDTO token(Authentication authentication) {
         LoginResponseDTO loginResponse = new LoginResponseDTO();
         LOG.info("Token requested for user: '{}'", authentication.getName());
         String token = tokenService.generateToken(authentication);
+        String refreshToken = tokenService.generateRefreshToken(authentication);
         LOG.info("Token granted: {}", token);
-        loginResponse.withUserName(authentication.getName()).withToken(token).withExpirationTime(6000).withUserId(1);
+        loginResponse.withUserName(authentication.getName()).withToken(token).withExpirationTime(6000).withUserId(1).withRefreshToken(refreshToken);
         return loginResponse;
     }
+
+
+
+
+
+
 
 }
--- src/main/java/com/isei/scod/ConfigSecurity/TokenService.java
+++ src/main/java/com/isei/scod/ConfigSecurity/TokenService.java
@@ -3,6 +3,7 @@ package com.isei.scod.ConfigSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,11 @@ public class TokenService {
 
     private final JwtEncoder encoder;
 
-    public TokenService(JwtEncoder encoder) {
+    private final JwtDecoder decoder;
+
+    public TokenService(JwtEncoder encoder, JwtDecoder decoder) {
         this.encoder = encoder;
+        this.decoder = decoder;
     }
 
     public String generateToken(Authentication authentication) {
@@ -33,6 +37,18 @@ public class TokenService {
                 .claim("scope", scope)
                 .build();
         return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+
+    public String generateRefreshToken(Authentication authentication) {
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder().expiresAt(now.plus(1, ChronoUnit.HOURS)).subject(authentication.getName()).build();
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    public Boolean authToken(Authentication authentication,String refreshToken){
+        String userName = this.decoder.decode(refreshToken).getClaimAsString("sub");
+        return authentication.getName().equals(userName);
     }
 
 }
